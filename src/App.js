5803f91878b8f0b3ec45fea65153475000a1932e
Alejandro Nanez <alejonanez@gmail.com>
2021-05-07 20:20:44 -0500

    Final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+
+/**
+ *
+ * @param {string} name - el nombre
+ * @param age
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function Greet({name, age}) {
+  if (!age) {
+    return <p>Hola {name.map()}</p>
+  }
+
+  return <p>Hola {name} tienes {age} años</p>;
+}
+
+
+function Container({children}) {
+  return (
+    <div>
+      <h1>I'm your navigation!</h1>
+      {children}
+      <h1>I'm your footer :)!</h1>
+    </div>
+  )
+}
+
+function Body({children}) {
+  return (
+    <div>
+      <p>lorem lorem lorem ipsum bla bla</p>
+      <p>lorem lorem lorem ipsum bla bla</p>
+      <p>lorem lorem lorem ipsum bla bla</p>
+      {children}
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Body>
+          <Greet name={33} age={30}/>
+        </Body>
+      </Container>
     </div>
   );
 }
