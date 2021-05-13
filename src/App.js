57dd8f18257bb1765255e45117adc2e43e99ba77
Simon Hoyos <shmesa23@gmail.com>
2021-05-12 20:12:52 -0500

    En vivo 2 listas y http

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import './App.css';
 
 /**
@@ -37,12 +39,53 @@ function Body({children}) {
   )
 }
 
+let chat = [
+  {
+    id: 1,
+    avatar: 'https://...',
+    username: 'simonhoyos',
+    content: 'Bienvinid@s',
+  },
+  {
+    id: 2,
+    avatar: 'https://...',
+    username: 'alejonanez',
+    content: 'hola a todos y todas',
+  },
+]
+
+function List() {
+  let elements = []
+
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      baseURL: 'https://swapi.dev/api',
+      url: '/people'
+    })
+      .then(response => {
+        console.log(response.data)
+      })
+  })
+
+  return chat.map(( message, index ) => {
+    return (
+      <article key={message.id}>
+        <p>{message.avatar}</p>
+        <h2>{message.username}</h2>
+        <p>{message.content}</p>
+      </article>
+    )
+  })
+}
+
 function App() {
   return (
     <div className="App">
       <Container>
         <Body>
           <Greet name={33} age={30}/>
+          <List />
         </Body>
       </Container>
     </div>
